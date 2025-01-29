a918ae176fcd1faa868e9b2006de56a42adcdc56
Ishika Walia <100034839+IshikaW1@users.noreply.github.com>
2025-01-29 14:49:24 +0530

    Create MlscLogo.jsx

---
+++ src/components/MlscLogo/MlscLogo.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./MlscLogo.module.css";
+
+const MlscLogo = () => {
+  return <div className={styles.MlscLogo}></div>;
+};
+
+export default MlscLogo;

4dab363d4c4c18ee7250096173347a51d7c2fa6f
Ishika Walia <100034839+IshikaW1@users.noreply.github.com>
2025-01-29 14:57:48 +0530

    Update MlscLogo.jsx

--- src/components/MlscLogo/MlscLogo.jsx
+++ src/components/MlscLogo/MlscLogo.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import styles from "./MlscLogo.module.css";
 
 const MlscLogo = () => {
-  return <div className={styles.MlscLogo}></div>;
+  return <div className={styles.MlscLogo}><img src="./public/mlsclogo.png" /> </div>;
 };
 
 export default MlscLogo;
+
